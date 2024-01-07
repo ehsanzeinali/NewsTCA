f99cb83a9c81268420b3e7e7853ee41ea7f9669a
Ehsan <ehsanzeinalinia@gmail.com>
2024-01-07 09:53:16 +0330

    first commit

---
+++ NewsTCA/NewsTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTCAApp.swift
+//  NewsTCA
+//
+//  Created by Ehsan Zeinalinia on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
